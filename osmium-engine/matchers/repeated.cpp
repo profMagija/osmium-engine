961c05327dcf8d8494a23b82e2b1b0941ef76a19
Stefan Nožinić <stefan@lugons.org>
2018-11-09 18:36:02 +0100

    repeated WIP

--- osmium-engine/matchers/repeated.cpp
+++ osmium-engine/matchers/repeated.cpp
@@ -9,11 +9,21 @@ namespace matchers
 {
     bool repeated::match(const std::vector<ref<value>> &target, bindings& matched) 
     {
-        pattern_->set_next(this);
-        if (! pattern_->match(target, matched)) return next()->match(target, matched);
-        else 
+        // TODO still does not work properly 
+        if (allow_null_)
         {
-            return true;
+
+            pattern_->set_next(this);
+            if (! pattern_->match(target, matched)) 
+                return next()->match(target, matched);
+            else 
+            {
+                set_matched(pattern_->get_matched());
+                return true;
+            }
+
+        } else {
+            
         }
     }
 }
